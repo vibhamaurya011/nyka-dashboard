653f22f1a54d46917c5aef88fc0c8dcf82937029
Shivam Maurya <vibhamaurya011@gmail.com>
2024-01-05 18:36:23 +0530

    done

--- server/index.js
+++ server/index.js
@@ -2,10 +2,14 @@ const express = require('express');
 const { connection } = require('./config/db');
 const { userRoute } = require('./routes/user.route');
 const { productRoute } = require('./routes/product.route');
+var cors = require('cors')
+
 require("dotenv").config()
 const port = process.env.PORT
 
+
 const app = express();
+app.use(cors())
 app.use(express.json())
 
 app.use("/", userRoute)
